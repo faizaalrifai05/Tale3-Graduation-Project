eee1991d3adc56792132775d9091a3fccf3df214
faizaalrifai05 <faizaalrifai05@gmail.com>
2026-04-11 19:06:12 +0300

    Replace car model text field with dropdown

--- lib/screens/driver/driver_vehicle_details_screen.dart
+++ lib/screens/driver/driver_vehicle_details_screen.dart
@@ -87,11 +87,12 @@ class DriverVehicleDetailsScreen extends StatelessWidget {
               ),
               const SizedBox(height: 32),
 
-              
-              
-              
-              _buildTextField('Car Model', 'e.g., Toyota Camry'),
+              // Form fields
+              _buildDropdownField('Car Make', 'Select Car Make'),
               const SizedBox(height: 16),
+              _buildDropdownField('Car Model', 'Select Car Model'),
+              const SizedBox(height: 16),
+              
               Row(
                 children: [
                   Expanded(
